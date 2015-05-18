100d056e37ff9b924729ae2fc694f9579be3d747
stranger <ivchenkodima@gmail.com>
2015-05-18 03:34:39 +0300

    arrays in ruby

--- first/app/controllers/say_controller.rb
+++ first/app/controllers/say_controller.rb
@@ -6,3 +6,18 @@ class SayController < ApplicationController
   def goodbye
   end
 end
+
+def say_goodnight(name)
+	result = 'Good night, '+ name
+	return result
+end
+
+puts say_goodnight("Dima")
+puts say_goodnight("Jade") 
+
+
+
+def  say(name)
+	"goodbye, #{name.capitalize}"
+end
+puts say_goodnight('ass')
