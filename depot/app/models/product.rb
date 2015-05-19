582a3a61ebaec79ed632ae6bf1f2d6dff52c1e90
stranger <ivchenkodima@gmail.com>
2015-05-20 00:39:25 +0300

    validates form

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ActiveRecord::Base
-	validates :title, :description, :image_url, presence: true
-	validates :price, numericality: {greater_then_or_equal_to: 0.01}
-	validates :title, uniqueness: true 
-	validates :image_url ,allow_blank: true, format: {
-		with: %r{\.(gif|jpg|png)\Z}i,
-		message: 'URL have to be in gif, jpg or png.'
-	}
+	 validates :title, :description, :image_url, presence: true
+	 validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	 validates :title, uniqueness: true
+	 validates :image_url, allow_blank: true, format: {
+	 	with: %r{\.(gif|jpg|png)\Z}i,
+	 	message: 'must be a URL for GIF, JPG or PNG image.'
+ # URL должен указывать на изображение формата GIF, JPG или PNG
+ }
 end
